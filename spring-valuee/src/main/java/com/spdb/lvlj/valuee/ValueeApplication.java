7a15cdb625309d6b5d9836daf85df88b61f9d4c3
lvlinjie <lvlinjie@yeah.net>
2017-11-08 16:49:01 +0800

    第一次提交工程代码

---
+++ spring-valuee/src/main/java/com/spdb/lvlj/valuee/ValueeApplication.java
@@ -0,0 +1,12 @@
+package com.spdb.lvlj.valuee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValueeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValueeApplication.class, args);
+	}
+}

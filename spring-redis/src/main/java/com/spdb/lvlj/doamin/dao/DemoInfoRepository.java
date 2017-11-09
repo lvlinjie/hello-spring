5d39f992a9dce752397715195e14b898dc67bfb5
lvlinjie <32391072+lvlinjie@users.noreply.github.com>
2017-11-09 15:54:17 +0800

    jpa实现

--- spring-redis/src/main/java/com/spdb/lvlj/doamin/dao/DemoInfoRepository.java
+++ spring-redis/src/main/java/com/spdb/lvlj/doamin/dao/DemoInfoRepository.java
@@ -4,7 +4,10 @@ import com.spdb.lvlj.doamin.pojo.DemoInfo;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
+/*
+*这是一个接口，没有具体的实现，这就是JPA,只要继承了这一个接口，就不用再写sql语句
+*JpaRepository<DemoInfo,Long>，第一个是pojo，第二个是这个Pojo的铸件类型
+*/
 public interface DemoInfoRepository extends JpaRepository<DemoInfo,Long> {
 
 }
